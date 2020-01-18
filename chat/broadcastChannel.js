a96160f12bb505e9ea86e7f8fadde3769db4eeeb
wkwksama <mailpakgasing@gmail.com>
2020-01-18 16:21:45 +0700

    fix bot message format: markdown

--- chat/broadcastChannel.js
+++ chat/broadcastChannel.js
@@ -2,7 +2,7 @@ const bot = require('./__bot');
 const config = require('../config');
 
 module.exports = (message) => {
-  bot.sendMessage(config.NODE_ENV === 'development' ? '@devdinusian' : '@dinusian', message).then(() => {
+  bot.sendMessage(config.NODE_ENV === 'development' ? '@devdinusian' : '@dinusian', message, { parse_mode: 'Markdown' }).then(() => {
     console.log('sent broadcast telegram channel');
   });
 };
