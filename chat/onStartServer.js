bfae3ebb68812e8ba50430b748d596ec8b9a0afa
wkwksama <mailpakgasing@gmail.com>
2020-01-18 15:20:55 +0700

    fix on start message

--- chat/onStartServer.js
+++ chat/onStartServer.js
@@ -2,7 +2,7 @@ const bot = require('./__bot');
 const config = require('../config');
 
 module.exports = () => {
-  bot.sendMessage(config.ADMIN_CHAT_ID, 'bot restarted').then((/* res */) => {
+  bot.sendMessage(config.ADMIN_CHAT_ID, `bot started: ${config.NODE_ENV}`).then((/* res */) => {
     console.log('bot restarted');
   });
 };
