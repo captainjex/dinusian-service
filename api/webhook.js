ce4c473e848c96e6e789119f55b85b79b45712ab
wkwksama <mailpakgasing@gmail.com>
2020-04-26 14:09:40 +0700

    webhook scrap ann

---
+++ api/webhook.js
@@ -0,0 +1,33 @@
+const scrapAnnouncement = require('../cron/scrapAnnouncement');
+const config = require('../config');
+
+module.exports = (req, res) => {
+  const isValidKey = req.query.key === config.WEBHOOK_KEY;
+  if (!isValidKey) {
+    return res.send({
+      error: true,
+      message: 'INVALID KEY'
+    });
+  }
+
+  switch (req.query.action) {
+    case 'SCRAP:ANNOUNCEMENTS':
+      scrapAnnouncement(null, (error) => {
+        if (error) {
+          return res.status(500).send({
+            error: true,
+            message: error.message,
+            data: error
+          });
+        }
+
+        return res.send({
+          error: false,
+          message: 'success'
+        });
+      });
+      break;
+    default:
+      break;
+  }
+};
--- config/index.js
+++ config/index.js
@@ -6,4 +6,5 @@ module.exports = {
   MONGO_STRING: process.env.MONGO_STRING || 'mongodb://localhost:27017/dinusian',
   TOKEN_TELEGRAM_BOT: process.env.TOKEN_TELEGRAM_BOT || 'yourtoken',
   ADMIN_CHAT_ID: process.env.ADMIN_CHAT_ID || 123456789,
+  WEBHOOK_KEY: process.env.WEBHOOK_KEY || 'lalala1234',
 };
